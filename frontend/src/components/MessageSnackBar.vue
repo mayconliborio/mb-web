049e3d2efc1543d71cd642662f643be6c8fd4993
Maycon Liborio <liborio.md@gmail.com>
2023-04-12 19:08:15 -0300

    feat(frontend): Add Snackbar component and add Pinia

---
+++ frontend/src/components/MessageSnackBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="show" class="snackbar-cover">
+    <div :class="['snackbar', type]">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useSnackbarStore } from '../store/snackbar.js';
+
+const snackbarStore = useSnackbarStore();
+
+const show = computed(() => snackbarStore.show);
+const message = computed(() => snackbarStore.message);
+const type = computed(() => snackbarStore.type);
+</script>
+
+<style scoped>
+.snackbar-cover {
+  position: absolute;
+  top: 60px;
+  right: 50px;
+  justify-content: center;
+  z-index: 9999;
+}
+.snackbar {
+  width: auto;
+  max-width: 400px;
+  top: 20px;
+  padding: 10px;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: bold;
+  transition: opacity 0.3s ease;
+}
+
+.success {
+  background-color: #28a745;
+}
+
+.error {
+  background-color: #dc3545;
+}
+
+.warning {
+  background-color: #ffc107;
+}
+</style>
---
+++ frontend/src/store/snackbar.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia';
+
+export const useSnackbarStore = defineStore({
+  id: 'snackbar',
+  state: () => ({
+    show: false,
+    message: '',
+    type: 'success',
+    timeoutId: null,
+  }),
+  actions: {
+    showSnackbar({ message, type }) {
+      this.message = message;
+      this.type = type;
+      this.show = true;
+
+      if (this.timeoutId !== null) {
+        clearTimeout(this.timeoutId);
+      }
+
+      this.timeoutId = setTimeout(() => {
+        this.show = false;
+        this.timeoutId = null;
+      }, 3000);
+    },
+  },
+});
