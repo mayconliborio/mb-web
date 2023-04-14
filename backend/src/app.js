9c2f40d7d4548a1ea553c82a8046aef1f6c73675
Maycon Liborio <liborio.md@gmail.com>
2023-04-14 17:56:13 -0300

    feat(backend): Add get endpoint registration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const router = require('./routes')
 
 const app = express()
 const PORT = 3000
 const HOST = '0.0.0.0'
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(cors())
 app.use(bodyParser.json())
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,5 +5,8 @@ const { registerUser, getAllUsers } = require('../controllers/userController')
 
 router.post('/registration', registerUser)
 router.get('/users', getAllUsers)
+router.get('/registration', (req, res) => {
+  res.render('registration', { tipoPessoa: '' })
+})
 
 module.exports = router
